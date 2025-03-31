04674c598b7b81c8a8fd06fb96555dfd5da5c525
yashpoojari09 <yashpoojari009@gmail.com>
2025-03-31 12:09:59 +0530

    git

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -2,8 +2,11 @@ import ForgotPasswordForm from "./ForgotPasswordForm";
 
 const ForgotPasswordPage=()=>{
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+    <div className="flex justify-center items-center min-h-screen bg-primary">
+      <div className="bg-dark p-6 rounded-lg shadow-lg w-96">
+      <h2 className="text-2xl font-bold text-white text-center mb-4">Forgot Password</h2>
       <ForgotPasswordForm />
+    </div>
     </div>
   );
 }
