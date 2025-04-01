f0f05c1edbdd6853629e30b59fdeda15ee767c6d
yashpoojari09 <yashpoojari009@gmail.com>
2025-04-01 13:19:09 +0530

    git

--- src/app/tenants/[tenantId]/taxation/AddTaxRuleForm.tsx
+++ src/app/tenants/[tenantId]/taxation/AddTaxRuleForm.tsx
@@ -37,8 +37,7 @@ export default function AddTaxRuleForm({ onClose }: { onClose: () => void }) {
           <div>
             <label className="block text-sm font-medium text-[#001e38]">Tax Rate</label>
             <input
-            type="number"
-              {...register("taxRate", { valueAsNumber: true })}
+              {...register("taxRate")}
               className="w-full border p-3 rounded text-[#001e38]"
             />
             {errors.taxRate && <p className="text-red-500 text-sm">{errors.taxRate.message}</p>}
