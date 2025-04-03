60b990e4763bab2c2145dc2c079c534de2693b44
yashpoojari09 <yashpoojari009@gmail.com>
2025-04-03 13:12:11 +0530

    ui

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,5 @@
 const config = {
-  theme: {
-    extend: {
-      boxShadow: {
-        lg: "0 3px 9px -3px #ffffff, 0 4px 6px -4px #0000001a",
-      },
-    },
-  },
+ 
   plugins: ["@tailwindcss/postcss"],
 };
 
